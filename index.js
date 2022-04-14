12becc5fb14fd299dd0c276914fa8df79c1ede08
KamerrEzz <kamerrezz@hotmail.com>
2022-04-14 16:12:03 -0500

    utils edit db

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 module.exports = {
     ZeewLevel: require('./src/Manager'),
+    ZeewLevelxpManager: require('./src/utils/xpManager'),
+    ZeewLevelConstant: require('./src/Constant'),
+    ZeewLevelVersion: require('./package.json').version
 }
